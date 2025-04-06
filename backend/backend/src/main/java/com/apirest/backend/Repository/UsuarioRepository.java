98b5ff8bff114f01968d8740ae4bda4fd8a65033
Sxm-dev4 <sammm204@outlook.com>
2025-04-05 19:26:42 -0500

    email

--- backend/backend/src/main/java/com/apirest/backend/Repository/UsuarioRepository.java
+++ backend/backend/src/main/java/com/apirest/backend/Repository/UsuarioRepository.java
@@ -3,8 +3,11 @@ import com.apirest.backend.Model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-    
+    Optional<Usuario> findByVerificacionEmail_Token(String token);
 } 
