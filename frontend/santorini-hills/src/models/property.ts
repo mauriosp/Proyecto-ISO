c88363b2ce3795d89c5c7f722eac96ed96fa7286
juanmayorquin <juanjmayor@outlook.es>
2025-04-06 12:42:13 -0500

    agregar interfaces de Advertisement y Property

---
+++ frontend/santorini-hills/src/models/property.ts
@@ -0,0 +1,7 @@
+export interface Property {
+    type: string; // e.g., "Apartment", "House", etc.
+    address: string;
+    area: number; // in square meters
+    bathrooms: number;
+    bedrooms: number;
+}
