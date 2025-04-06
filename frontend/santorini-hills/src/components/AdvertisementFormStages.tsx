60e4b5d3e04a2eed1dad09a5eb4ca6dbf6e4af24
juanmayorquin <juanjmayor@outlook.es>
2025-04-06 12:42:34 -0500

    agregar el formulario de anuncios y la página de publicación de anuncios

---
+++ frontend/santorini-hills/src/components/AdvertisementFormStages.tsx
@@ -0,0 +1,23 @@
+export const AdvertisementTypeStage = () => {
+    return (
+        <form className="" onSubmit={(e) => e.preventDefault()}>
+            <h2>¿Cuál de estas opciones describe mejor tu espacio?</h2>
+        </form>
+    );
+};
+
+export const PropertyDetailsStage = () => {
+    return (
+        <form className="flex flex-col space-y-4" onSubmit={(e) => e.preventDefault()}>
+            
+        </form>
+    );
+};
+
+export const PublishReviewStage = () => {
+    return (
+        <div className="flex flex-col space-y-4">
+            
+        </div>
+    );
+};
